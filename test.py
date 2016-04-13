f2061a60a5bcf0c5b58efea4bc8b2fc096f06861
remerjohnson <ryan.emerson.johnson@gmail.com>
2016-04-13 16:01:00 -0700

    Took some info out of test script

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from twarc import Twarc
 
-client_key = 'qkyfU7RwPyDAvcn7KpdVeLvGC'
-client_secret = 'ng3MiWHh2pfRERdCgR2Ob0zRzrMi06LKgcY42SgXPPuyiBvu2p'
+client_key = 'client_key'
+client_secret = 'client_secret'
 access_token = '197456523-m2qIYWxkQTFKj0ModTQPcdByTnjryHwLRm9L8o5y'
-access_token_secret = 'sTT7MqYIOWPZHEZiub8ppTQUCPNIxnjkMaAiAxa0126rp'
+access_token_secret = 'access_token_secret'
 
 t = Twarc(client_key, client_secret, access_token, access_token_secret)
 for tweet in t.search("resigncameron"):
